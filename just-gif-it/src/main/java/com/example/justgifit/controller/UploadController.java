68886b51dcb9a9f8ba667cb4c81f8fadbdfd60dd
Vigya Singh <vigyasingh123@gmail.com>
2020-01-25 23:39:21 -0500

    Adding controller upload

---
+++ just-gif-it/src/main/java/com/example/justgifit/controller/UploadController.java
@@ -0,0 +1,41 @@
+package com.example.justgifit.controller;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class UploadController {
+	
+	private static final Logger log = LoggerFactory.getLogger(UploadController.class);
+
+	
+	@Value("${multipart.location}")
+	private String  location;
+	
+	@RequestMapping(value = "/upload", method = RequestMethod.POST, produces = MediaType.IMAGE_GIF_VALUE)
+	public String upload(@RequestPart("file") MultipartFile file,
+			@RequestParam("start") int start, 
+			@RequestParam("end") int end,
+			@RequestParam("speed") int speed,
+			@RequestParam("repeat") boolean repeat) throws IllegalStateException, IOException {
+		java.io.File videoFile = new  java.io.File(location + "/" + System.currentTimeMillis() +".mp4");
+		file.transferTo(videoFile);
+		
+		log.info("Saved file to {}" + videoFile.getAbsolutePath());
+		
+		return "";
+		
+		
+	}
+		
+}
